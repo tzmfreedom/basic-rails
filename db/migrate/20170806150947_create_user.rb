6d0c01d4d3a7edee663ef0f5cfbd8373f31c93d2
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-01-07 00:25:15 +0900

    Update schema

--- db/migrate/20170806150947_create_user.rb
+++ db/migrate/20170806150947_create_user.rb
@@ -1,15 +1,15 @@
 class CreateUser < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string "nickname"
-      t.string "email"
-      t.string "password_digest"
-      t.boolean "active", default: true
-      t.boolean "email_verified", default: false, null: false
-      t.string "email_verify_token"
-      t.datetime "email_verify_token_sent_at"
-      t.string "reset_password_token"
-      t.datetime "reset_password_token_sent_at"
+      t.string :nickname
+      t.string :email
+      t.string :password_digest
+      t.boolean :active, default: true
+      t.boolean :email_verified, default: false, null: false
+      t.string :email_verify_token, unique: true
+      t.datetime :email_verify_token_sent_at
+      t.string :reset_password_token, unique: true
+      t.datetime :reset_password_token_sent_at
 
       t.timestamps
     end
